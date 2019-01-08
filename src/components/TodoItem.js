2f909b3e0a248fb76de1af2047b627e9074661d8
Shiyu Wang <shiyu3169@gmail.com>
2019-01-08 15:02:19 -0500

    proptypes

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export default class TodoItem extends Component {
   render() {
@@ -9,3 +10,7 @@ export default class TodoItem extends Component {
     );
   }
 }
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+};
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import TodoItem from "./TodoItem";
+import PropTypes from "prop-types";
 
 export default class Todos extends Component {
   render() {
     return this.props.todos.map(todo => <TodoItem key={todo.id} todo={todo} />);
   }
 }
+
+// PropTypes
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
+};

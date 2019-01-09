ffb9de098313afdf113c27fe3f27a94a770a1786
Shiyu Wang <shiyu3169@gmail.com>
2019-01-09 09:35:10 -0500

    mark for compelete

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,13 +4,23 @@ import PropTypes from "prop-types";
 export default class TodoItem extends Component {
   getStyle = () => {
     return {
+      background: "#f4ff4",
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
       textDecoration: this.props.todo.completed ? "line-through" : "none"
     };
   };
   render() {
+    const { id, title } = this.props.todo;
     return (
       <div style={this.getStyle()}>
-        <p>{this.props.todo.title}</p>
+        <p>
+          <input
+            type="checkbox"
+            onChange={this.props.markComplete.bind(this, id)}
+          />{" "}
+          {title}
+        </p>
       </div>
     );
   }
